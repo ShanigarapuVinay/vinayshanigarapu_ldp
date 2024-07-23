614dc41e9be7bb2af7fd83b918016923aa4ae262
ShanigarapuVinay <vinay.shanigarapu@zemosolabs.com>
2024-07-23 12:57:28 +0530

    Initial Commit - Add Component Task replicating given component

---
+++ component-assignment/src/Shared/Typography/TypographyProps.ts
@@ -0,0 +1,6 @@
+export interface TypographyProps {
+  mainTextVariant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  mainText: string;
+  subTextVariant: "body1" | "body2" | "subtitle1" | "subtitle2" | "caption";
+  subText: string;
+}
